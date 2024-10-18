54c276c42b387c5c425536cfae1ba4ddb06e5614
Amin Aliyev <aliyevamin972@gmail.com>
2024-10-18 14:38:08 +0400

    step 2

--- happy family step 1/src/main/java/org/example/Human.java
+++ happy family step 1/src/main/java/org/example/Human.java
@@ -1,28 +1,39 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 
 public class Human {
-    String name;
-    String surname;
-    int year;
-    int iq;
-    Pet pet;
-    Human mother;
-    Human father;
-    String[][] schedule;
+    private String name;
+    private String surname;
+    private int year;
+    private int iq;
+    private Pet pet;
+    private Human mother;
+    private Human father;
+    private String[][] schedule;
+    private Family family;
 
-    // Empty constructor
+    static {
+        System.out.println("Class Human is being loaded");
+    }
+
+    {
+        System.out.println("A new Human object is created");
+    }
+
+    //Empty constructor
     public Human() {}
 
-    // Constructor with name, surname, and year
+    //Constructor with name, surname, and year
     public Human(String name, String surname, int year) {
         this.name = name;
         this.surname = surname;
         this.year = year;
     }
 
-    // Constructor with name, surname, year, father, and mother
+    //Constructor with name, surname, year, father, and mother
     public Human(String name, String surname, int year, Human father, Human mother) {
         this.name = name;
         this.surname = surname;
@@ -31,7 +42,7 @@ public class Human {
         this.mother = mother;
     }
 
-    // Constructor with all fields
+    //Constructor with all fields
     public Human(String name, String surname, int year, int iq, Pet pet, Human father, Human mother, String[][] schedule) {
         this.name = name;
         this.surname = surname;
@@ -42,46 +53,104 @@ public class Human {
         this.mother = mother;
         this.schedule = schedule;
     }
-
-    // Method for greeting the pet
-    public void greetPet() {
-        System.out.println("Hello, " + pet.nickname);
+    //getters and setters
+    public String getName() {
+        return name;
     }
 
-    // Method to describe the pet
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public String[][] getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String[][] schedule) {
+        this.schedule = schedule;
+    }
+
+    public Family getFamily() {
+        return family;
+    }
+
+    public void setFamily(Family family) {
+        this.family = family;
+    }
+
+
+    //Method for greeting the pet
+    public void greetPet() {
+        System.out.println("Hello, " + pet.getNickname());
+    }
+
+    //Method to describe the pet
     public void describePet() {
-        String slynessOfPet = pet.trickLevel > 50 ? "very sly" : "almost not sly";
-        System.out.println("I have a " + pet.species + ", it is " + pet.age + " years old, it is " + slynessOfPet);
+        String slynessOfPet = pet.getTrickLevel() > 50 ? "very sly" : "almost not sly";
+        System.out.println("I have a " + pet.getSpecies() + ", it is " + pet.getAge() + " years old, it is " + slynessOfPet);
     }
 
 
     public boolean feedPet(boolean timeToFeed) {
         if (timeToFeed) {
-            System.out.println("Hm... I will feed " + name + "'s " + pet.species);
+            System.out.println("Hm... I will feed " + name + "'s " + pet.getSpecies());
             return true;
         } else {
             Random random = new Random();
             int randomValue = random.nextInt(101); // Generate a number between 0-100
-            if (pet.trickLevel > randomValue) {
-                System.out.println(" I have to feed " + name + "'s " + pet.species);
+            if (pet.getTrickLevel() > randomValue) {
+                System.out.println(" I have to feed " + name + "'s " + pet.getSpecies());
                 return true;
             } else {
-                System.out.println("I think " + name + "'s " + pet.species + " is not hungry.");
+                System.out.println("I think " + name + "'s " + pet.getSpecies() + " is not hungry.");
                 return false;
             }
         }
     }
-    // Overriding of the toString() method
+    //Overriding of the toString(), hashcode() and equals() methods
     @Override
     public String toString() {
-
         return "Human{name='" + name +
                 "', surname='" + surname +
                 "', year=" + year +
                 ", iq=" + iq +
-                ", mother=" + mother.name + mother.surname +
-                ", father=" + father.name + father.surname +
-                ", pet=" + pet +
-                "}";
+                ", schedule="
+                + Arrays.deepToString(schedule) + "}";
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Human)) return false;
+        Human human = (Human) o;
+        return year == human.year && iq == human.iq && Objects.equals(name, human.name) && Objects.equals(surname, human.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, year, iq);
     }
 }
--- happy family step 1/src/main/java/org/example/Pet.java
+++ happy family step 1/src/main/java/org/example/Pet.java
@@ -1,18 +1,26 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Pet {
-    String species;
-    String nickname;
-    int age;
-    int trickLevel;
-    String[] habits;
+    private String species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
+    static {
+        System.out.println("Class Pet is being loaded");
+    }
 
-    // Emoty constructor
+    {
+        System.out.println("A new Pet object is created");
+    }
+
+    //Emoty constructor
     public Pet() {}
 
-    // Constructor with species and nickname
+    //Constructor with species and nickname
     public Pet(String species, String nickname) {
         this.species = species;
         this.nickname = nickname;
@@ -27,22 +35,64 @@ public class Pet {
         this.habits = habits;
     }
 
-    // Method displays a message 'I am eating'
+    //getters and setters
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+
+    //Method displays a message 'I am eating'
     public void eat() {
         System.out.println("I am eating");
     }
 
-    // method displays a message 'Hello, owner. I am - [the name of the pet]. I miss you!'
+    //method displays a message 'Hello, owner. I am - [the name of the pet]. I miss you!'
     public void respond() {
         System.out.println("Hello, owner. I am - " + nickname + ". I miss you!");
     }
 
-    // method displays a message 'I need to cover it up'
+    //method displays a message 'I need to cover it up'
     public void foul() {
         System.out.println("I need to cover it up");
     }
 
-    // Overriding of the toString() method
+    //Overriding of the toString(), equals() and hashcode methods
     @Override
     public String toString() {
         return species + "{nickname='" + nickname +
@@ -50,5 +100,17 @@ public class Pet {
                 ", trickLevel=" + trickLevel +
                 ", habits=" + Arrays.toString(habits) +
                 "}";
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pet)) return false;
+        Pet pet = (Pet) o;
+        return age == pet.age && trickLevel == pet.trickLevel && Objects.equals(species, pet.species) && Objects.equals(nickname, pet.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(species, nickname, age, trickLevel);
     }
 }
